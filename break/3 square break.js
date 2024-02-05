896fac4e548789a7732ce4e52f86e6ec81ecce85
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-05 18:24:33 +0600

    all break task completed

--- break/3 square break.js
+++ break/3 square break.js
@@ -1,3 +1,10 @@
 /* 
 Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
 */
+for (let i = 1; i <= 100; i++) {
+    if (i * i === Math.pow(i, 2)) {
+        console.log(`First square number encounters is ${i}`);
+        break;
+    }
+    console.log(i);
+}
