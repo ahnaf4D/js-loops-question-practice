63fe4e13a791b3f24c8e9ed3ee91cdd8e9a92f0e
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-05 18:13:10 +0600

    Getting stuck with loops break

--- break/2 sum upto 100.js
+++ break/2 sum upto 100.js
@@ -1,3 +1,14 @@
 /* 
 Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
 */
+let item = 1;
+let sum = 0;
+while (item <= 100) {
+    item++;
+    sum += item;
+    // When results 100 loop will exit
+    if (sum === 100) {
+        break;
+    }
+}
+console.log(sum);
